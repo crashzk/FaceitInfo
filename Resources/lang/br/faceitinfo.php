aa29b0515f6a1740abf95d1ec40413cdfa635382
crashzk <crashzk@zkservidores.com>
2026-03-28 08:26:10 -0300

    Create faceitinfo.php

---
+++ Resources/lang/br/faceitinfo.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'title' => [
+        'list' => 'Perfil Faceit',
+        'description' => 'Página de configurações para exibição de informações do Faceit',
+    ],
+    'labels' => [
+        'api_key' => 'API Key',
+        'game' => 'Jogo',
+    ],
+    'placeholders' => [
+        'api_key' => 'Digite sua API Key',
+        'game' => 'Selecione um jogo',
+    ],
+    'popovers' => [
+        'api_key' => 'Você pode obter uma API Key em <a href="https://developers.faceit.com" target="_blank">sua conta Faceit</a>',
+        'game' => 'Selecione o jogo para o qual este módulo será utilizado',
+    ],
+    'buttons' => [
+        'save' => 'Salvar',
+    ],
+    'messages' => [
+        'failed' => 'Falha ao salvar as configurações',
+        'saved' => 'Configurações salvas com sucesso',
+    ],
+
+    'level' => 'Level',
+    'elo' => 'ELO',
+    'matches' => 'PARTIDAS',
+    'win_rate' => 'TAXA DE VITÓRIA',
+    'name' => 'NOME',
+    'region' => 'REGIÃO',
+    'headshots' => 'HEADSHOTS',
+    'kd_ratio' => 'K/D',
+    'win_streak' => 'SEQUÊNCIA DE VITÓRIAS',
+    'current_streak' => 'SEQUÊNCIA ATUAL',
+    'wins' => 'VITÓRIAS',
+    'last_matches' => 'ÚLTIMAS PARTIDAS',
+    'updated_at' => 'Atualizado',
+    'recent_matches' => 'PARTIDAS RECENTES',
+];
